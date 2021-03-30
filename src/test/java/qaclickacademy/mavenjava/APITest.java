008fa6c5655dfdde903f25c81c6abb88f38e0449
Pranoti <nargidepranoti@gmail.com>
2021-03-30 23:47:48 +0530

    first commit

---
+++ src/test/java/qaclickacademy/mavenjava/APITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+@Test
+	public void postJira()
+	{
+	System.out.println("postJira");
+	}
+@Test
+public void deleteTwitter()
+{
+	System.out.println("deleteTwitter");	
+}
+}
---
+++ src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("SeleniumTest");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
